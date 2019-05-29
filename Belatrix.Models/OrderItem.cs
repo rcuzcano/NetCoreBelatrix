4641f56a8cc94083b37c14f288188bc9db672c2c
Cesar Velarde <juvega@gmail.com>
2019-05-29 14:40:45 -0500

    Add project files.

---
+++ Belatrix.Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Belatrix.WebApi.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
