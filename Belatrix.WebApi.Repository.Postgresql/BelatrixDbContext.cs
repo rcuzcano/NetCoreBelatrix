fece4c75be0b1c203e86f78d66a2074c05513d87
Cesar Velarde <juvega@gmail.com>
2019-05-29 18:58:22 -0500

    Avance clases.

---
+++ Belatrix.WebApi.Repository.Postgresql/BelatrixDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Belatrix.WebApi.Models;
+using Belatrix.WebApi.Repository.Postgresql.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgresql
+{
+    public class BelatrixDbContext : DbContext
+    {
+        public BelatrixDbContext(DbContextOptions<BelatrixDbContext> opt)
+            :base(opt)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfig());
+        }
+    }
+}
---
+++ Belatrix.WebApi.Repository.Postgresql/Configurations/CustomerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Belatrix.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgresql.Configurations
+{
+    internal class CustomerConfig : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("customer");
+
+            builder.Property(p => p.Id)
+                .HasColumnName("id")
+                .UseNpgsqlIdentityColumn();
+
+            builder.Property(p => p.FirstName)
+                .HasColumnName("first_name")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.HasIndex(e => new { e.LastName, e.FirstName })
+                .HasName("customer_name_idx");
+        }
+    }
+}
