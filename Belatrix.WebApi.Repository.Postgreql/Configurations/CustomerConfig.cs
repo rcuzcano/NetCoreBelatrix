55b68e14de7a3228fad53d7849bfa373f970061f
rcuzcano <randy.cuzcano@gmail.com>
2019-05-29 19:00:27 -0500

    Clase 4

---
+++ Belatrix.WebApi.Repository.Postgreql/Configurations/CustomerConfig.cs
@@ -0,0 +1,44 @@
+﻿using Belatrix.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgreql.Configurations
+{
+    internal class CustomerConfig : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("customer");
+
+            builder.Property(p => p.Id)
+                .HasColumnName("id")
+                .UseNpgsqlIdentityColumn();
+
+            builder.Property(p => p.LastName)
+                .HasColumnName("last_name")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.Property(p => p.City)
+                .HasColumnName("city")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.Property(p => p.Country)
+                .HasColumnName("country")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.Property(p => p.Phone)
+                .HasColumnName("phone")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.HasIndex(e => new { e.FirstName, e.LastName })
+                .HasName("customer_name_idx");
+        }
+    }
+}
---
+++ Belatrix.WebApi.Repository.Postgreql/Configurations/OrderConfig.cs
@@ -0,0 +1,35 @@
+﻿using Belatrix.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgreql.Configurations
+{
+    internal class OrderConfig : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("order");
+
+            builder.Property(p => p.CustomerId)
+                .HasColumnName("id")
+                .IsRequired();
+
+            builder.Property(p => p.OrderDate)
+                .HasColumnName("date")
+                .IsRequired();
+
+            builder.Property(p => p.OrderNumber)
+                .HasColumnName("number")
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(p => p.TotalAmount)
+                .HasColumnName("total_amount")
+                .IsRequired();
+
+        }
+    }
+}
